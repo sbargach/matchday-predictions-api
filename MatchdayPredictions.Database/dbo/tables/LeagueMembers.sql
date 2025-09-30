e6f260a56223a25a95da9cd5bb7246f2f1321884
Souff <soufyanbargach04@gmail.com>
2025-09-30 19:55:37 +0200

    Add database project and first tables

---
+++ MatchdayPredictions.Database/dbo/tables/LeagueMembers.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE dbo.LeagueMembers
+(
+    LeagueId INT NOT NULL,
+    UserId   INT NOT NULL,
+    JoinedUtc DATETIME2(0) NOT NULL CONSTRAINT DF_LeagueMembers_JoinedUtc DEFAULT (SYSUTCDATETIME()),
+
+    CONSTRAINT PK_LeagueMembers PRIMARY KEY (LeagueId, UserId),
+    CONSTRAINT FK_LeagueMembers_League FOREIGN KEY (LeagueId) REFERENCES dbo.Leagues(Id),
+    CONSTRAINT FK_LeagueMembers_User   FOREIGN KEY (UserId)   REFERENCES dbo.Users(Id)
+);
+GO
---
+++ MatchdayPredictions.Database/dbo/tables/Leagues.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE dbo.Leagues
+(
+    Id          INT IDENTITY(1,1) NOT NULL CONSTRAINT PK_Leagues PRIMARY KEY,
+    Name        NVARCHAR(100) NOT NULL,
+    Code        NVARCHAR(20)  NOT NULL CONSTRAINT UX_Leagues_Code UNIQUE, -- simple join code
+    CreatedUtc  DATETIME2(0)  NOT NULL CONSTRAINT DF_Leagues_CreatedUtc DEFAULT (SYSUTCDATETIME())
+);
+GO
