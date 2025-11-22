17e252f746b8500d2e664b13d03d3f76b5850377
Souff <soufyanbargach04@gmail.com>
2025-11-22 15:49:22 +0100

    fix integrationtest

--- MatchdayPredictions.Api.IntegrationTests/Infrastructure/IntegrationTestFixture.cs
+++ MatchdayPredictions.Api.IntegrationTests/Infrastructure/IntegrationTestFixture.cs
@@ -1,5 +1,6 @@
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 using Testcontainers.MsSql;
 using Xunit;
 
@@ -10,6 +11,7 @@ public sealed class IntegrationTestFixture : IAsyncLifetime
     private const string JwtKey = "integration-tests-secret-key";
     private const string JwtIssuer = "MatchdayPredictions.IntegrationTests";
     private const string JwtAudience = "MatchdayPredictions.IntegrationTests";
+    private const string DatabaseName = "MatchdayPredictions";
 
     private readonly MsSqlContainer _dbContainer;
 
@@ -18,13 +20,12 @@ public sealed class IntegrationTestFixture : IAsyncLifetime
         _dbContainer = new MsSqlBuilder()
             .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
             .WithPassword("Str0ngP@ssw0rd!")
-            .WithDatabase("MatchdayPredictions")
             .Build();
     }
 
     public CustomWebApplicationFactory Factory { get; private set; } = null!;
 
-    public string ConnectionString => _dbContainer.GetConnectionString();
+    public string ConnectionString { get; private set; } = null!;
 
     public HttpClient CreateClient() => Factory.CreateClient();
 
@@ -33,6 +34,8 @@ public sealed class IntegrationTestFixture : IAsyncLifetime
     public async Task InitializeAsync()
     {
         await _dbContainer.StartAsync();
+
+        ConnectionString = await EnsureDatabaseAsync();
 
         var migrator = new DatabaseMigrator(ConnectionString);
         await migrator.ApplyMigrationsAsync();
@@ -50,5 +53,33 @@ public sealed class IntegrationTestFixture : IAsyncLifetime
     {
         var seeder = CreateSeeder();
         return seeder.ClearDataAsync();
+    }
+
+    private async Task<string> EnsureDatabaseAsync()
+    {
+        var baseBuilder = new SqlConnectionStringBuilder(_dbContainer.GetConnectionString());
+        var adminConnectionString = new SqlConnectionStringBuilder(baseBuilder.ConnectionString)
+        {
+            InitialCatalog = "master"
+        }.ConnectionString;
+
+        var databaseConnectionString = new SqlConnectionStringBuilder(baseBuilder.ConnectionString)
+        {
+            InitialCatalog = DatabaseName
+        }.ConnectionString;
+
+        await using var connection = new SqlConnection(adminConnectionString);
+        await connection.OpenAsync();
+
+        var createCommand = connection.CreateCommand();
+        createCommand.CommandText = $"""
+            IF DB_ID('{DatabaseName}') IS NULL
+            BEGIN
+                CREATE DATABASE [{DatabaseName}];
+            END
+            """;
+        await createCommand.ExecuteNonQueryAsync();
+
+        return databaseConnectionString;
     }
 }
